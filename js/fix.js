b343d2bdb348f77522166ac7300e8b4b56dc5807
YouranonymousFriend <jan-mueller-1@web.de>
2020-10-23 11:37:01 +0200

    Added icons and support for the odysee platform.

--- js/fix.js
+++ js/fix.js
@@ -1,4 +1,19 @@
 var isonfixed = 0;
+var selectedPlatform = 'bitchute';
+
+document.getElementById("selectPlatform").addEventListener('change', () => {
+
+	selectedPlatform = document.getElementById('selectPlatform').value;
+	
+	if(selectedPlatform == 'bitchute') {
+		document.getElementById('fixVideo').style.display = "block";
+		document.getElementById('bcExtrasContainer').style.display = "block";
+	} else {
+		document.getElementById('fixVideo').style.display = "none";
+		document.getElementById('bcExtrasContainer').style.display = "none";
+	}
+	
+});
 
 document.getElementById("fixVideo").addEventListener('click', () => {
 
@@ -18,29 +33,48 @@ document.getElementById("fixVideo").addEventListener('click', () => {
 });
 
 document.getElementById("dlVideo").addEventListener('click', () => {
+	
+	selectedPlatform = document.getElementById('selectPlatform').value;
 
     function modifyDOM() {
         //You can play with your DOM here or check URL against your regex
         return document.getElementById('player').innerHTML;
     }
+	
+	function modifyDOMOdysee() {
+        //You can play with your DOM here or check URL against your regex
+		return document.getElementById('vjs_video_3_html5_api').src;
+    }
 
-    //We have permission to access the activeTab, so we can call browser.tabs.executeScript:
-    browser.tabs.executeScript({
-        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
-		if(results == null) {
-			isonfixed = 1;
-		}
-		if(isonfixed == 0) {
-			fixedUrl = searchBuggyVideo(results[0]);
+	if(selectedPlatform == 'bitchute') {
+		//We have permission to access the activeTab, so we can call browser.tabs.executeScript:
+		browser.tabs.executeScript({
+			code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+		}, (results) => {
+			if(results == null) {
+				isonfixed = 1;
+			}
+			if(isonfixed == 0) {
+				fixedUrl = searchBuggyVideo(results[0]);
+				document.getElementById('download').href = fixedUrl;
+				document.getElementById('download').style.display = 'block';
+				document.getElementById('download').click();
+			} else {
+				currentLocation = browser.tabs.query({currentWindow: true, active: true}).then(logTabs, console.error);
+			}
+		});
+	} else {
+		browser.tabs.executeScript({
+			code: '(' + modifyDOMOdysee + ')();' //argument here is a string but function.toString() returns function's code
+		}, (results) => {		
+			fixedUrl = results[0]+'.mp4';
 			document.getElementById('download').href = fixedUrl;
+			document.getElementById("download").type = "video/mp4";
 			document.getElementById('download').style.display = 'block';
 			document.getElementById('download').click();
-		} else {
-			currentLocation = browser.tabs.query({currentWindow: true, active: true}).then(logTabs, console.error);
-		}
-    });
-	
+			
+		});
+	}
 });
 
 // verbose variant
